6fe9a76af8d214f74ef43d3c17d94bb5d87f19fd
siddharthsharma94 <sharmasiddharthcs@gmail.com>
2020-10-01 02:11:55 -0700

    Update template.ts

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -81,7 +81,7 @@ function getCss(theme: string, fontSize: string) {
     .logo {
         margin: 0 75px;
         border-radius: 64px
-        box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.25);
+        box-shadow: 0px 4px 40px rgba(0, 0, 0, 0.45);
     }
 
     .plus {
